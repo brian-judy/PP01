1ca4ead7fbce1e6400fb31f69fc0c58d37495228
zvang <99816387+zvang@users.noreply.github.com>
2022-02-16 15:13:56 -0700

    Update Direction.java

--- PP1/src/Direction.java
+++ PP1/src/Direction.java
@@ -15,11 +15,51 @@ public class Direction extends Thread{
 	// This is the code part that needs to be programmed by students to solve the maze 
 	// using the provided path/direction algorithm
 	
-	
         public void run(){
+
+	// This is a SAMPLE code for moving the student image in the maze
+	// and updates the information in Position.java GUI class, append text into the JTextArea object
+	// you should delete/update this code and start your solution, you may just keep the part of updating the information
+	// in the Position.java class, appending information into the JTextArea object
 		
-	//THIS IS MY PROPOSED SOLUTION -- ZACH
-	  while(!maze.isDone()) {
+        //PART II
+        	
+        boolean evenCol;
+        
+        //Check if column is even or odd
+        while(!maze.isDone()) {
+        	if(maze.getCurrCol() % 2 == 0) {
+        		evenCol = true;
+        	}
+        	else {
+        		evenCol = false;
+        	}
+        
+        	//Set column movement
+        	if(evenCol && this.maze.moveDown()) {
+        		this.maze.moveDown();
+        		location.textArea.append("Col: " + maze.getCurrCol() + " Down" + "\n");
+        	}
+        	else if (!evenCol && this.maze.moveUp()){
+        		this.maze.moveUp();
+        		location.textArea.append("Col: " + maze.getCurrCol() + " UP" + "\n");
+        	}
+        	
+        	this.maze.moveRight();
+        	location.textArea.append("Moved to row " + maze.getCurrRow() + ", column " + maze.getCurrCol() + "\n");
+  
+        	
+        }
+        	
+        
+        	
+        	
+        
+        //***************************************************************************************************************
+        
+        /* PART I
+        
+        while(!maze.isDone()) {
         	if(this.maze.moveRight()) {
         	location.textArea.append("Moved to row " + maze.getCurrRow() + ", column " + maze.getCurrCol() + "\n");
         	}
@@ -27,39 +67,25 @@ public class Direction extends Thread{
         		location.textArea.append("Moved to row " + maze.getCurrRow() + ", column " + maze.getCurrCol() + "\n");
         		}
         }
-		
+        
         maze.isDone();
-	
-	/* BRIAN'S ORIGINAL CODE
-	this.maze.moveRight();
-        this.maze.moveRight();
-        this.maze.moveRight();
-        this.maze.moveRight();
-        this.maze.moveRight();
-        this.maze.moveRight();
-        this.maze.moveRight();
-        this.maze.moveRight();
-        this.maze.moveRight();
-        this.maze.moveDown();
-        this.maze.moveDown();
-	
-		
-	// This is a SAMPLE code for moving the student image in the maze
-	// and updates the information in Position.java GUI class, append text into the JTextArea object
-	// you should delete/update this code and start your solution, you may just keep the part of updating the information
-	// in the Position.java class, appending information into the JTextArea object
-		
-		while(!maze.isDone()) {
+        
+        	
+        //PROVIDED SAMPLE CODE
+        while(!maze.isDone()) {
 				
 		if(this.maze.moveDown())
 		location.textArea.append("Moved to row " + maze.getCurrRow() + ", column " + maze.getCurrCol() + "\n");
+		
+		
 				
 		}
-	*/
+		*/
 		
-		location.textArea.append("Logo Found \n");
+		//location.textArea.append("Logo Found \n");
 		
 	}
    
 	
 }
+
