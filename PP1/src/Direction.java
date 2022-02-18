aca417db3cebe514d69b8771dc3323223fcc00c1
brian-judy <99777650+brian-judy@users.noreply.github.com>
2022-02-17 21:58:56 -0700

    Update Direction.java

--- PP1/src/Direction.java
+++ PP1/src/Direction.java
@@ -24,9 +24,66 @@ public class Direction extends Thread{
 	// in the Position.java class, appending information into the JTextArea object
 		
 		while(!maze.isDone()) {
+		
+			//check if current column is even or odd
+			if(maze.getCurrCol() % 2 == 0)
+				isEven = true;
+			else
+				isEven = false;
+			
+			//move down on even columns and up on odd columns
+			if(isEven && maze.moveDown()) {
+				maze.moveDown();
+				location.textArea.append("Moved to row " + maze.getCurrRow() + ", column " + maze.getCurrCol() + "\n");
+
+			}
+			else if(!isEven && maze.moveUp()) {
+				maze.moveUp();
+				location.textArea.append("Moved to row " + maze.getCurrRow() + ", column " + maze.getCurrCol() + "\n");
+			}
+			
+			//if you reach the bottom or top row and cannot move up or down, move right.
+			if(maze.getHeight() == maze.getCurrRow() && !maze.moveUp() && !maze.moveDown()) {
+				maze.moveRight();
+				location.textArea.append("Moved to row " + maze.getCurrRow() + ", column " + maze.getCurrCol() + "\n");
+			}
+			else if(maze.getCurrRow() == 0 && !maze.moveUp() && !maze.moveDown()) {
+				maze.moveRight();
+				location.textArea.append("Moved to row " + maze.getCurrRow() + ", column " + maze.getCurrCol() + "\n");
+			}
+			
+			
+			//encounter obstacle while moving down in even columns
+			else if(!maze.moveDown() && isEven) {
+				if(maze.moveRight())
+					location.textArea.append("Moved to row " + maze.getCurrRow() + ", column " + maze.getCurrCol() + "\n");
+				if(maze.moveDown())
+					location.textArea.append("Moved to row " + maze.getCurrRow() + ", column " + maze.getCurrCol() + "\n");
+				if(maze.moveDown())
+					location.textArea.append("Moved to row " + maze.getCurrRow() + ", column " + maze.getCurrCol() + "\n");
+				if(maze.moveLeft())
+					location.textArea.append("Moved to row " + maze.getCurrRow() + ", column " + maze.getCurrCol() + "\n");
+			}
+			//encounter obstacle moving up in odd columns
+			else if(!maze.moveUp() && !isEven) {
+				if(maze.moveLeft())
+					location.textArea.append("Moved to row " + maze.getCurrRow() + ", column " + maze.getCurrCol() + "\n");
+				if(maze.moveUp())
+					location.textArea.append("Moved to row " + maze.getCurrRow() + ", column " + maze.getCurrCol() + "\n");
+				if(maze.moveUp())
+					location.textArea.append("Moved to row " + maze.getCurrRow() + ", column " + maze.getCurrCol() + "\n");
+				if(maze.moveRight())
+					location.textArea.append("Moved to row " + maze.getCurrRow() + ", column " + maze.getCurrCol() + "\n");
+			}
+			
+			// end maze if the last cell is reached
+			if((maze.getCurrRow() == maze.getHeight()) && (maze.getCurrCol() == maze.getWidth())) {
+				location.textArea.append("Last cell reached.");
+				break;
+			}
 				
-		if(this.maze.moveDown())
-		location.textArea.append("Moved to row " + maze.getCurrRow() + ", column " + maze.getCurrCol() + "\n");
+		//if(this.maze.moveDown())
+		//location.textArea.append("Moved to row " + maze.getCurrRow() + ", column " + maze.getCurrCol() + "\n");
 				
 		}	
 		
